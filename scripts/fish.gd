cf634e9a8dfba10a9311815c42314799a4766a47
einardarri <git@einardarri.is>
2024-11-28 12:46:20 +0000

    add fish

---
+++ scripts/fish.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+var origin : Vector2
+@export var speed : float = .025
+@export_range(0,360,0.1,"radians_as_degrees") var angle : float = 0.0
+@export var radius : int = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not origin:
+		origin = global_position
+		
+	radius *= 10
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var current_rotation : Vector2 = Vector2(cos(angle) * radius, sin(angle) * radius)
+	set_position(current_rotation + origin)
+	angle += speed
+	set_rotation(angle+((90*PI)/180))
+	if position.y >= origin.y:
+		flip_v = true
+	else:
+		flip_v = false
+		
