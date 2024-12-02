e5dcbbf4986f71908806e809dec4eeb0f35b4c13
einardarri <git@einardarri.is>
2024-12-02 14:41:29 +0000

    some collider bs

---
+++ scripts/hook.gd
@@ -0,0 +1,64 @@
+extends StaticBody2D
+class_name hook
+
+var player: Player = null
+var hook_sprite : Sprite2D = null
+
+# the possision of the hook when it is cast
+var pos := Vector2(0,0)
+
+var is_cast : bool = false
+
+@export var button_id : MouseButton = 1
+
+@export_category("Hook power")
+@export var min_power : int = 50
+@export var max_power : int = 150
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = GameManager.get_player()
+	hook_sprite = $hookSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# lock the hook rotation
+	global_rotation = 0.0
+	
+	# makes sure that the hook does not move with the player when cast
+	if is_cast:
+		set_global_position(pos)
+
+
+func _input(event: InputEvent) -> void:
+	# check if a mouse button was pressed
+	if event is InputEventMouseButton:
+		# check if it is the button we want and that it was pressed down
+		if event.get_button_index() == button_id and event.is_pressed():
+			if !is_cast:
+				# Cast the hook
+				var direction = Vector2(1,0).rotated($"..".rotation)
+				# TODO need to make something better for this as there is no control over how far yo cast the hook
+				var hook_force = (min_power + max_power) / 2
+				
+				is_cast = true
+				
+				hook_sprite.visible = is_cast
+				var col : KinematicCollision2D = move_and_collide(direction * hook_force)
+				pos = get_global_position()
+				print(pos)
+				if col:
+					print(col.get_collider().get_class())
+					
+				
+				
+			else:
+				# Retract the hook
+				is_cast = false
+				
+				hook_sprite.visible = is_cast
+				
+				set_position(Vector2(0,0))
+				pos = Vector2(0,0)
+				
--- scripts/rod_swivel.gd
+++ scripts/rod_swivel.gd
@@ -1,54 +1,6 @@
 extends Node2D
 
-var player: Player = null
-
-
-var hook : StaticBody2D = null
-var hook_sprite : Sprite2D = null
-var hook_collider : CollisionShape2D
-
-var is_cast : bool = false
-
-@export var button_id : MouseButton = 1
-
-@export_category("Hook power")
-@export var min_power : int = 50
-@export var max_power : int = 150
-
-func _ready() -> void:
-	player = GameManager.get_player()
-	hook = $hook
-	hook_sprite = $hook/hookSprite2D
-	hook_collider = $hook/hookCollisionShape2D
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
-	# lock the hook rotation
-	hook.global_rotation = 0.0
-	hook_collider.enab
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.get_button_index() == button_id and event.is_pressed():
-			if !is_cast:
-				# Cast the hook
-				var direction =  Vector2(1,0)
-				var hook_force = randf_range(min_power, max_power)
-				
-				is_cast = true
-				
-				hook_sprite.visible = is_cast
-				var col : KinematicCollision2D = hook.move_and_collide(direction * hook_force)
-				
-				hook_collider.is_coll
-				
-			else:
-				# Retract the hook
-				is_cast = false
-				
-				hook_sprite.visible = is_cast
-				
-				hook.set_position(Vector2(0,0))
-				
