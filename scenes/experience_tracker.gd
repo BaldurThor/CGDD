8693a134f0d88cf52b19acd1d9f1f4b6ed891224
VDavidK <vidir98@gmail.com>
2024-12-02 17:25:35 +0000

    Add dynamite thrower weapon, dynamite projectile, sound effects for experience pickup and more

--- scenes/experience_tracker.gd
+++ scenes/experience_tracker.gd
@@ -6,10 +6,13 @@ var current_experience: int = 0
 var required_for_level_up: int = 10
 signal gain_experience(amount: int)
 signal update_experience_bar(experience: int)
+@onready var pickup_sound_effect: AudioStreamPlayer2D = $PickupSoundEffect
 
 func _ready() -> void:
 	gain_experience.connect(_gain_experience)
 
 func _gain_experience(amount: int) -> void:
 	current_experience += amount
+	pickup_sound_effect.pitch_scale = clamp(1.0 + (float(current_experience) / float(required_for_level_up)), 0.1, 2)
+	pickup_sound_effect.play()
 	update_experience_bar.emit(current_experience)
---
+++ scripts/dynamite.gd
@@ -0,0 +1,39 @@
+class_name Dynamite extends RigidBody2D
+
+## The time before the bullet automatically despawns
+@export var despawn_delay: float
+
+@onready var despawn_timer: Timer = $DespawnTimer
+@onready var area_2d: Area2D = $Area2D
+@onready var explosion_animation: AnimationPlayer = $ExplosionAnimation
+
+var direction: Vector2
+var _weapon_type: WeaponType
+var hit_count: int = 0
+
+func init(weapon_type: WeaponType, bullet_direction: Vector2) -> void:
+	_weapon_type = weapon_type
+	direction = bullet_direction
+
+func _ready() -> void:
+	despawn_timer.wait_time = despawn_delay
+	despawn_timer.timeout.connect(_explode)
+
+func _physics_process(_delta: float) -> void:
+	linear_velocity = direction.normalized() * _weapon_type.projectile_speed
+
+func _on_body_entered(body: Node) -> void:
+	if body is Enemy:
+		direction = Vector2.ZERO
+
+func _on_despawn_timer_timeout() -> void:
+	_explode()
+
+func _explode() -> void:
+	var nearby_enemies = area_2d.get_overlapping_bodies()
+	# Randomly pitch the sound effect up/down to add variety.
+	#explosion_sound_effect.pitch_scale = randf_range(0.5, 2)
+	explosion_animation.play("explode")
+	for enemy in nearby_enemies:
+		if enemy is Enemy:
+			enemy.take_damage(_weapon_type.damage * _weapon_type.damage_effectiveness)
