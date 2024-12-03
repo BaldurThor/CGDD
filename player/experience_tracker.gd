d71372c32f8b2f74da7cca382e35498334b8cef5
einardarri <git@einardarri.is>
2024-12-03 13:17:49 +0000

    add xp scaling on level up

--- player/experience_tracker.gd
+++ player/experience_tracker.gd
@@ -3,7 +3,8 @@ class_name Experience extends Node
 # Manages everything related to experience on the player-side
 
 var current_experience: int = 0
-var required_for_level_up: int = 10
+var current_level : int = 0
+var required_for_level_up: int = xp_needed_form(0)
 
 signal gain_experience(amount: int)
 signal update_experience_bar(experience: int)
@@ -13,6 +14,16 @@ signal level_up
 func _ready() -> void:
 	gain_experience.connect(_gain_experience)
 
+
+func xp_needed_form(level : int) -> int:
+	"""
+	does some funky math 
+	to find out how much xp is needed
+	to level up to the next level
+	"""
+
+	return (level ** 2) + (3 * level) + 10
+
 func _gain_experience(amount: int) -> void:
 	current_experience += amount
 	pickup_sound_effect.pitch_scale = clamp(1.0 + (float(current_experience) / float(required_for_level_up)), 0.1, 2)
@@ -20,5 +31,9 @@ func _gain_experience(amount: int) -> void:
 	update_experience_bar.emit(current_experience)
 	
 	if current_experience >= required_for_level_up:
-		required_for_level_up *= 2.5
+		current_level+=1
+		current_experience -= required_for_level_up
+		update_experience_bar.emit(current_experience)
+		required_for_level_up = xp_needed_form(current_level)
+		print(required_for_level_up)
 		level_up.emit()
