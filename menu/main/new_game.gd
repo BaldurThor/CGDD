03400b5672d2e54b25f598fbf7d71b58dbe5ad62
Baldur <baldur@BaldursIdeapad>
2024-12-03 13:10:20 +0000

    ui/ux stuff

--- menu/main/new_game.gd
+++ menu/main/new_game.gd
@@ -1,15 +1,6 @@
 extends Label
 
 @export var run: PackedScene
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_pressed():
--- menu/pause/continue.gd
+++ menu/pause/continue.gd
@@ -1,18 +1,10 @@
 extends Label
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+@export var pause_node: Node
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_pressed():
-		get_node("/root/Main/Pause").hide()
+		pause_node.hide()
 		get_tree().paused = false
 
 
