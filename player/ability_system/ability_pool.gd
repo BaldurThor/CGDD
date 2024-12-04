714644261e603c15505c1c121866c0679d91331b
Axel Snaer <hab123321123@gmail.com>
2024-12-04 16:45:22 +0000

    Improve ability picking system

--- player/ability_system/ability_pool.gd
+++ player/ability_system/ability_pool.gd
@@ -2,5 +2,46 @@ class_name AbilityPool extends Resource
 
 @export var abilities: Array[AbilityInfo] = []
 
-func pick_ability() -> AbilityInfo:
-	return abilities.pick_random()
+var global_ability_pool: Array[AbilityInfo] = []
+var pick_counts: Dictionary = {}
+
+# Workaround for not having a _ready() function in the resource
+var initialized: bool = false
+
+func _pick_ability(ability_pool: Array[AbilityInfo]) -> AbilityInfo:
+	while ability_pool.size() > 0:
+		return ability_pool.pop_front()
+	
+	return null
+
+func _get_ability_pool() -> Array[AbilityInfo]:
+	# TODO: Make this shuffle based on rarity. Least rare should generally be
+	# placed more in the front.
+	var ability_pool = global_ability_pool.duplicate()
+	ability_pool.shuffle()
+	return ability_pool
+
+func get_ability_selection(count: int = 3) -> Array[AbilityInfo]:
+	if !initialized:
+		global_ability_pool = abilities.duplicate()
+		initialized = true
+	
+	var ability_pool = _get_ability_pool()
+	var picked_abilities: Array[AbilityInfo] = []
+	
+	for i in range(min(count, ability_pool.size())):
+		picked_abilities.push_back(ability_pool.pop_front())
+	
+	return picked_abilities
+
+func add_ability_pick_count(ability: AbilityInfo):
+	var pick_count = pick_counts.find_key(ability)
+		
+	if pick_count == null:
+		pick_counts[ability] = 0
+	
+	pick_counts[ability] += 1
+	
+	if ability.max_amount > 0 and pick_counts[ability] >= ability.max_amount:
+		var ability_index: int = global_ability_pool.find(ability)
+		global_ability_pool.remove_at(ability_index)
--- player/hud/ability_choice/ability_picker.gd
+++ player/hud/ability_choice/ability_picker.gd
@@ -2,18 +2,30 @@ class_name AbilityPicker extends VBoxContainer
 
 @onready var ability_selection: HBoxContainer = $AbilitySelection
 @onready var ability_system: AbilitySystem = %AbilitySystem
+@onready var backlog_count: Label = $HBoxContainer/BacklogCount
 
 const ABILITY_CHOICE = preload("res://player/hud/ability_choice/ability_choice.tscn")
+
+var backlog: int = 0
 
 func _ready() -> void:
 	visible = false
 
-func display_choice(ability1: AbilityInfo, ability2: AbilityInfo, ability3: AbilityInfo):
+func refresh_choices():
+	for child in ability_selection.get_children():
+		child.queue_free()
+	
+	if backlog == 0:
+		visible = false
+		get_tree().paused = false
+		return
+	
 	visible = true
 	
-	add_choice(ability1)
-	add_choice(ability2)
-	add_choice(ability3)
+	var choices = ability_system.loot_table.get_ability_selection()
+	
+	for choice in choices:
+		add_choice(choice)
 
 func add_choice(ability: AbilityInfo):
 	var choice: AbilityChoice = ABILITY_CHOICE.instantiate()
@@ -22,9 +34,14 @@ func add_choice(ability: AbilityInfo):
 	ability_selection.add_child(choice)
 
 func pick_ability(ability: AbilityInfo):
-	for child in ability_selection.get_children():
-		child.queue_free()
-	
-	visible = false
 	ability_system.add_ability(ability)
-	get_tree().paused = false
+	backlog -= 1
+	backlog_count.text = "%d" % [backlog]
+	refresh_choices()
+
+func _on_experience_level_up() -> void:
+	backlog += 1
+	backlog_count.text = "%d" % [backlog]
+	
+	if backlog == 1:
+		refresh_choices()
