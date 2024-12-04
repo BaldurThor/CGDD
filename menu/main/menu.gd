702dc02af3afaebb7247f1f06fe9899c5c9e79f3
Baldur <baldur@DESKTOP-SV8AVGC.>
2024-12-04 13:33:37 +0000

    store

--- menu/main/menu.gd
+++ menu/main/menu.gd
@@ -3,9 +3,6 @@ extends MarginContainer
 @export var info_node: Node
 @export var options_node: Node
 
-func _ready() -> void:
-	get_tree().paused = false
-
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		if info_node.visible:
