93d5ddccfa8cb59338d9d0e11dbe30030d30c57a
einardarri <git@einardarri.is>
2024-12-05 16:17:18 +0000

    global constants

--- consts.gd
+++ consts.gd
@@ -1,3 +1,4 @@
 extends Node
 
-enum TargetPriority { CLOSEST, FARTHEST, RANDOM, STRONGEST }
+enum TargetPriority { CLOSEST, FARTHEST, RANDOM, STRONGEST, WEEKEST}
+# WEEKEST is the enemy that has the lowest health if there are enemies that have the same health it will pick the closer of them
