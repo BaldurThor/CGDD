c941a1063ff2e5b321eaf1f945435316d8a04b04
Einar Darri <54663445+EinarDarri@users.noreply.github.com>
2024-12-05 16:34:05 +0000

    Update consts.gd
    Co-authored-by: VDavidK <35979804+VDavidK@users.noreply.github.com>

--- consts.gd
+++ consts.gd
@@ -1,4 +1,4 @@
 extends Node
 
-enum TargetPriority { CLOSEST, FARTHEST, RANDOM, STRONGEST, WEEKEST}
+enum TargetPriority { CLOSEST, FARTHEST, RANDOM, STRONGEST, WEAKEST}
 # WEEKEST is the enemy that has the lowest health if there are enemies that have the same health it will pick the closer of them
