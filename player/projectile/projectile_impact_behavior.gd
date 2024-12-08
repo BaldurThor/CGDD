342de7939226483600245f6cab4910edc5f389c7
VDavidK <vidir98@gmail.com>
2024-12-08 23:25:06 +0000

    Nerf explosive base radius. Fix soot having no art

--- player/projectile/projectile_impact_behavior.gd
+++ player/projectile/projectile_impact_behavior.gd
@@ -6,13 +6,14 @@ class_name ProjectileImpactBehavior extends Node
 @export var despawn_on_hit: bool = true
 ## If despawn_on_hit is false, this timer will determine the projectile's lifetime.
 @export var time_before_despawn: float = 0.0
+@export var can_pierce: bool = true
 
 var collision_count: int = 0
-var can_pierce: bool
+var max_pierce_count: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	can_pierce = projectile.weapon_type.pierce_count > 0
+	max_pierce_count = projectile.weapon_type.pierce_count + projectile.player_stats.extra_projectile_pierce
 	projectile.body_entered.connect(_handle_impact)
 	if !despawn_on_hit and time_before_despawn > 0.0:
 		get_tree().create_timer(time_before_despawn).timeout.connect(_on_timeout)
@@ -27,7 +28,7 @@ func _handle_impact(collided_with: Node2D) -> void:
 			projectile.collision_layer = 0
 			projectile.collision_mask = 0
 		collision_count += 1
-		if despawn_on_hit or projectile.weapon_type.pierce_count == collision_count - 1:
+		if despawn_on_hit or (can_pierce and max_pierce_count == collision_count - 1):
 			projectile.despawn.emit()
 
 func _on_timeout() -> void:
