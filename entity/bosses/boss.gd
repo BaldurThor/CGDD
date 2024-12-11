0f20a6187095692f47abd921e85b925671727a60
Axel Snaer <hab123321123@gmail.com>
2024-12-11 02:16:44 +0000

    Replace C'thulhu sprite + fix boss death

--- entity/bosses/boss.gd
+++ entity/bosses/boss.gd
@@ -1,10 +1,13 @@
 class_name Boss extends Enemy
 
 @export var spawn_offset: Vector2 = Vector2(0.0, -100.0)
+@onready var shadow: Sprite2D = $Shadow
 
 func _ready() -> void:
 	GameManager.add_boss(self)
-	entity_stats.death.connect(_on_death)
+	enemy_base.destroy_object.connect(_on_death)
 
 func _on_death() -> void:
 	GameManager.boss_killed.emit(self)
+	shadow.visible = false
+	queue_free()
