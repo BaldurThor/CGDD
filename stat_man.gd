1ff4ed04076657f7a2df89de325f4598793c1f9b
Axel Snaer <hab123321123@gmail.com>
2024-12-12 21:41:53 +0000

    Additional ui improvements

--- stat_man.gd
+++ stat_man.gd
@@ -4,20 +4,20 @@ class_name StatsMan extends Node
 
 var is_valid : bool = true
 
-var _total_damage_taken : int = 0
-var _total_damage_done : int = 0
-var _kills : int = 0
-var _abilities_picked : int = 0 
-var _boss_kills : int = 0
-var _time_played : int = 0
+var _total_damage_taken: int = 0
+var _total_damage_done: int = 0
+var _kills: int = 0
+var _abilities_picked: int = 0 
+var _boss_kills: int = 0
+var _time_played: int = 0
 # the time when you win, ueed for endless mode
-var _time_when_done : int = 0
-var _total_xp : int = 0
-var _heal_amount_regen : int = 0
-var _heal_amount_medkit : int = 0
-var _crits : int = 0
-var _caught_fish : int = 0
-var _shots_fired : int = 0
+var _time_when_done: int = 0
+var _total_xp: int = 0
+var _heal_amount_regen: int = 0
+var _heal_amount_medkit: int = 0
+var _crits: int = 0
+var _caught_fish: int = 0
+var _shots_fired: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,18 +40,6 @@ func _ready() -> void:
 
 func _over() -> void:
 	_time_when_done = _time_played
-	print("_total_damage_taken : ", _total_damage_taken)
-	print("_total_damage_done : ", _total_damage_done)
-	print("_kills : ", _kills)
-	print("_abilities_picked : ", _abilities_picked) 
-	print("_boss_kills : ",_boss_kills)
-	print("_time_played : ",_time_played)
-	print("_total_xp : ",_total_xp)
-	print("_heal_amount_regen : ",_heal_amount_regen)
-	print("_heal_amount_medkit : ",_heal_amount_medkit)
-	print("_crits : ",_crits)
-	print("_caught_fish : ",_caught_fish)
-	print("_shots_fired : ",_shots_fired)
 
 func cheat() -> void:
 	is_valid = false
@@ -95,6 +83,39 @@ func _got_crit() -> void:
 	
 func _made_projectile() -> void:
 	_shots_fired += 1
-	
-	
-	
+
+func get_total_damage_taken() -> int:
+	return _total_damage_taken
+
+func get_total_damage_done() -> int:
+	return _total_damage_done
+
+func get_kills() -> int:
+	return _kills
+
+func get_abilities_picked() -> int:
+	return _abilities_picked 
+
+func get_boss_kills() -> int:
+	return _boss_kills
+
+func get_time_when_done() -> int:
+	return _time_when_done
+
+func get_total_xp() -> int:
+	return _total_xp
+
+func get_heal_amount_regen() -> int:
+	return _heal_amount_regen
+
+func get_heal_amount_medkit() -> int:
+	return _heal_amount_medkit
+
+func get_crits() -> int:
+	return _crits
+
+func get_caught_fish() -> int:
+	return _caught_fish
+
+func get_shots_fired() -> int:
+	return _shots_fired
