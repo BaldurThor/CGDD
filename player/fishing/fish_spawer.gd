650f3b6c9e966fe74cb7a474e686b3fac51f7dca
einardarri <git@einardarri.is>
2024-12-12 12:18:55 +0000

    fish stuff

--- player/fishing/fish_spawer.gd
+++ player/fishing/fish_spawer.gd
@@ -1,5 +1,5 @@
-extends Node
-class_name FishSpawner
+@tool
+class_name FishSpawner extends Node2D
 
 var player : Player
 
@@ -7,8 +7,15 @@ var player : Player
 @export var fish_scene : PackedScene
 
 @export_group("Spawn Range")
-@export var radius_max : int = 50
-@export var radius_min : int = 5
+@export var radius_max: int:
+	set(value):
+		radius_max = value
+		queue_redraw()
+		
+@export var radius_min: int:
+	set(value):
+		radius_min = value
+		queue_redraw()
 
 @export_group("Total amount of fish")
 # The maximun amount of fish on the level at any one time
@@ -34,13 +41,18 @@ func spawn_fish(cords : Vector2) -> void:
 	fish.set_position(cords)
 	
 	add_child(fish)
-	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# get the player
 	player = GameManager.get_player()
 	var start_fish_ammount : int = randi_range(minimum, maximum)
+	#print(start_fish_ammount)
 	for a in start_fish_ammount: 
 		var cords = gen_cord() + player.position
 		spawn_fish(cords)
+		
+func _draw() -> void:
+	return
+	draw_circle($"../Player".position,radius_max,Color.DARK_SEA_GREEN,true,5)
+	draw_circle($"../Player".position,radius_min,Color.DARK_RED,true,5)
