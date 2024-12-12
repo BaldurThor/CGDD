6e97c1d83bb5a2d53033214ab4ff173d4e353cbe
Baldur <baldur@DESKTOP-SV8AVGC.>
2024-12-12 16:31:38 +0000

    experience rework

--- entity/enemy/enemy_drops/experience/experience_gem.gd
+++ entity/enemy/enemy_drops/experience/experience_gem.gd
@@ -14,16 +14,23 @@ func _physics_process(_delta: float) -> void:
 	debug_label.visible = Debug.enable
 	if Debug.enable:
 		debug_label.text = "xp value : " + str(experience_value) + "\nTime to despawn : " + str(int(despawn_timer.time_left))
-	if experience_value < 11:
+	if experience_value < 26:
 		sprite.frame = 0
-	elif experience_value < 21:
+	elif experience_value < 51:
 		sprite.frame = 1
-	elif experience_value < 31:
+	elif experience_value < 76:
 		sprite.frame = 2
-		glow.material.set_shader_parameter("glow_color", Color(1.,0.,0.,1.))
-	elif experience_value > 30:
+		glow.material.set_shader_parameter("glow_color", Color(0.,1.,0.,1.))
+	elif experience_value < 101:
 		sprite.frame = 3
+		glow.material.set_shader_parameter("glow_color", Color(0.,1.,0.,1.))
+	elif experience_value < 151:
+		sprite.frame = 4
 		glow.material.set_shader_parameter("glow_color", Color(1.,0.,0.,1.))
+	elif experience_value > 150:
+		sprite.frame = 5
+		glow.material.set_shader_parameter("glow_color", Color(1.,0.,0.,1.))
+	
 	if should_track:
 		linear_velocity = (GameManager.get_player().global_position - global_position).normalized() * get_tracking_speed()
 	glow.material.set_shader_parameter("intensity", float(experience_value))
