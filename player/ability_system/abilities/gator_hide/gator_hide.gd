2988bc3f9bec5b1ba86d8e835df8db4bb5a4fad8
VDavidK <vidir98@gmail.com>
2024-12-14 15:12:51 +0000

    re-tag all abilities where appropriate

--- player/ability_system/abilities/gator_hide/gator_hide.gd
+++ player/ability_system/abilities/gator_hide/gator_hide.gd
@@ -1,5 +1,4 @@
 extends Ability
 
 func apply_effects(player_stats: PlayerStats) -> void:
-	player_stats.max_health += 5
-	player_stats.max_health_mod += 0.03
+	player_stats.max_health_mod += 0.12
--- player/weapons/melee/melee_weapon.gd
+++ player/weapons/melee/melee_weapon.gd
@@ -15,6 +15,7 @@ signal _attack_signal(enemy: Enemy)
 func _ready() -> void:
 	_attack_signal.connect(_attack)
 	melee_sprite.texture = melee.weapon_type.sprite
+	melee_sprite.scale = melee.weapon_type.sprite_scale
 	attack_timer.wait_time = melee.weapon_type.attack_speed / (melee.player_stats.attack_speed_mod * melee.player_stats.melee_attack_speed_mod)
 	attack_timer.timeout.connect(_on_attack_timer_timeout)
 	attack_timer.start()
