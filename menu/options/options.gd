f513644186be806fcb6c70d124e6608bdd05612c
Axel Snaer <hab123321123@gmail.com>
2024-12-15 00:52:07 +0000

    Update newspaper article

--- menu/options/options.gd
+++ menu/options/options.gd
@@ -5,6 +5,7 @@ extends TextureRect
 @onready var master_slider: HSlider = $Label/Master/MasterSlider
 @onready var sfx_slider: HSlider = $Label/Effects/EffectsSlider
 @onready var music_slider: HSlider = $Label/Music/MusicSlider
+@onready var screenshake_slider: HSlider = $Label/Screenshake/ScreenshakeSlider
 
 @export var options: Label
 
@@ -16,6 +17,7 @@ func _ready() -> void:
 	master_slider.value = SaveManager.master_volume
 	sfx_slider.value = SaveManager.sfx_volume
 	music_slider.value = SaveManager.music_volume
+	screenshake_slider.value = SaveManager.screenshake_amount
 	AudioServer.set_bus_volume_db(master_bus, linear_to_db(SaveManager.master_volume))
 	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(SaveManager.sfx_volume))
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(SaveManager.music_volume))
@@ -41,3 +43,7 @@ func _on_effects_slider_value_changed(value: float) -> void:
 func _on_music_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
 	SaveManager.music_volume = value
+
+
+func _on_screenshake_slider_value_changed(value: float) -> void:
+	SaveManager.screenshake_amount = value
--- save_manager.gd
+++ save_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var master_volume: float
 var sfx_volume: float
 var music_volume: float
+var screenshake_amount: float
 
 var master_bus = AudioServer.get_bus_index("Master")
 var sfx_bus = AudioServer.get_bus_index("Effects")
@@ -13,12 +14,14 @@ const OPTIONS_FILE: String = "user://options.json"
 const OPTIONS_MASTER_VOLUME_KEY = "master_volume"
 const OPTIONS_SFX_VOLUME_KEY = "sfx_volume"
 const OPTIONS_MUSIC_VOLUME_KEY = "music_volume"
+const OPTIONS_SCREENSHAKE_AMOUNT_KEY = "screenshake_amount"
 
 func _ready() -> void:
 	if not FileAccess.file_exists(OPTIONS_FILE):
 		master_volume = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
 		sfx_volume = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 		music_volume = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+		screenshake_amount = 1.0
 		return
 	
 	load_data()
@@ -33,9 +36,25 @@ func load_data() -> void:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
 		return
 	
-	master_volume = json.data[OPTIONS_MASTER_VOLUME_KEY]
-	sfx_volume = json.data[OPTIONS_SFX_VOLUME_KEY]
-	music_volume = json.data[OPTIONS_MUSIC_VOLUME_KEY]
+	if OPTIONS_MASTER_VOLUME_KEY in json.data:
+		master_volume = json.data[OPTIONS_MASTER_VOLUME_KEY]
+	else:
+		master_volume = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
+	
+	if OPTIONS_SFX_VOLUME_KEY in json.data:
+		sfx_volume = json.data[OPTIONS_SFX_VOLUME_KEY]
+	else:
+		sfx_volume = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
+	
+	if OPTIONS_MUSIC_VOLUME_KEY in json.data:
+		music_volume = json.data[OPTIONS_MUSIC_VOLUME_KEY]
+	else:
+		music_volume = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	
+	if OPTIONS_SCREENSHAKE_AMOUNT_KEY in json.data:
+		screenshake_amount = json.data[OPTIONS_SCREENSHAKE_AMOUNT_KEY]
+	else:
+		screenshake_amount = 1.0
 
 func save_data() -> void:
 	var save_file = FileAccess.open(OPTIONS_FILE, FileAccess.WRITE)
@@ -43,4 +62,5 @@ func save_data() -> void:
 		OPTIONS_MASTER_VOLUME_KEY: master_volume,
 		OPTIONS_SFX_VOLUME_KEY: sfx_volume,
 		OPTIONS_MUSIC_VOLUME_KEY: music_volume,
+		OPTIONS_SCREENSHAKE_AMOUNT_KEY: screenshake_amount,
 	}))
