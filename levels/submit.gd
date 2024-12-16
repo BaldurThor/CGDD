9edebf56ff29bf5fcb1825cbeeec5dfb3505c6c2
einardarri <git@einardarri.is>
2024-12-16 01:05:26 +0000

    add username

--- levels/submit.gd
+++ levels/submit.gd
@@ -7,10 +7,13 @@ func _ready() -> void:
 	end_stats.submit.connect(send)
 	
 	
-func send() -> void:
+func send(username : String) -> void:
 	if end_stats.is_valid == false:
 		return
-	var body = JSON.new().stringify(end_stats.get_dict())
+		
+	var dict = end_stats.get_dict()
+	dict["username"] = username
+	var body = JSON.new().stringify(dict)
 	var error = request(consts.LEADER_BOARD_URL,["Content-Type:application/json"],HTTPClient.METHOD_POST,body)
 
 	if error != OK:
